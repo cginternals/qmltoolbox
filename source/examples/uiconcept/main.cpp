6e52b094f97af0fcb7d7de61b51bd1f2de1057fb
Stefan Buschmann <stefan.buschmann@hpi.de>
2017-05-14 00:16:25 +0200

    Create qmltoolbox::Application that takes care of initializing qmltoolbox to avoid code duplication in examples

--- source/examples/uiconcept/main.cpp
+++ source/examples/uiconcept/main.cpp
@@ -1,39 +1,29 @@
 
-#include <QGuiApplication>
 #include <QTranslator>
 
+#include <qmltoolbox/Application.h>
 #include <qmltoolbox/qmltoolbox-version.h>
-#include <qmltoolbox/QmlApplicationEngine.h>
-#include <qmltoolbox/MessageHandler.h>
 
 
 int main(int argc, char *argv[])
 {
-    qmltoolbox::MessageHandler::instance().installMessageHandlers();
+    // Initialize QmlToolbox
+    qmltoolbox::Application::initialize();
 
-#ifdef QMLTOOLBOX_QT57
-    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
-
-    QGuiApplication app(argc, argv);
+    // Create application
+    qmltoolbox::Application app(argc, argv);
     app.setOrganizationName(QMLTOOLBOX_AUTHOR_ORGANIZATION);
     app.setOrganizationDomain("cginternals.com");
     app.setApplicationName(QString(QMLTOOLBOX_PROJECT_NAME) + "_uiconcept");
 
-    // Create QtQuick engine
-    qmltoolbox::QmlApplicationEngine engine;
-
     // Setup Localization
     QTranslator translator;
-    translator.load(QLocale::system(), "uiconcept", ".", engine.qmlToolboxModulePath() + "/examples/uiconcept/i18n");
+    translator.load(QLocale::system(), "uiconcept", ".", app.qmlEngine().qmlToolboxModulePath() + "/examples/uiconcept/i18n");
     app.installTranslator(&translator);
 
     // Load and show QML
-    engine.load(QUrl::fromLocalFile(engine.qmlToolboxModulePath() + "/examples/uiconcept/Main.qml"));
+    app.loadQml(app.qmlEngine().qmlToolboxModulePath() + "/examples/uiconcept/Main.qml");
 
     // Run application
-    const auto res = app.exec();
-
-    // Stop application
-    return res;
+    return app.exec();
 }
---
+++ source/qmltoolbox/source/Application.cpp
@@ -0,0 +1,49 @@
+
+#include <qmltoolbox/Application.h>
+
+#include <QUrl>
+
+#include <qmltoolbox/MessageHandler.h>
+
+    
+namespace qmltoolbox
+{
+
+
+void Application::initialize()
+{
+    // Redirect output to message handler
+    qmltoolbox::MessageHandler::instance().installMessageHandlers();
+
+    // Enable high DPI (if available)
+#ifdef QMLTOOLBOX_QT57
+    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+}
+
+Application::Application(int & argc, char ** argv)
+: QGuiApplication(argc, argv)
+{
+}
+
+Application::~Application()
+{
+}
+
+void Application::loadQml(const QString & path)
+{
+    m_qmlEngine.load(QUrl::fromLocalFile(path));
+}
+
+const QmlApplicationEngine & Application::qmlEngine() const
+{
+    return m_qmlEngine;
+}
+
+QmlApplicationEngine & Application::qmlEngine()
+{
+    return m_qmlEngine;
+}
+
+
+} // namespace qmltoolbox
